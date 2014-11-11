d44a44460715a0490e102591e2c990025fb4c9ea
alain sanchez <alainsanchez@MacBook-Air-de-alain.local>
2014-11-11 15:31:53 +0100

    Initial Commit

---
+++ Git test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git test
+//
+//  Created by alain sanchez on 11/11/14.
+//  Copyright (c) 2014 alain sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git test
+//
+//  Created by alain sanchez on 11/11/14.
+//  Copyright (c) 2014 alain sanchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
