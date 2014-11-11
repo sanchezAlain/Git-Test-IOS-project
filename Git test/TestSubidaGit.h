98e0c1e5fc4a2a3a7391d62d5539bd182ca2a5a5
alain sanchez <alainsanchez@MacBook-Air-de-alain.local>
2014-11-11 15:52:22 +0100

    added a test class

---
+++ Git test/TestSubidaGit.h
@@ -0,0 +1,13 @@
+//
+//  TestSubidaGit.h
+//  Git test
+//
+//  Created by alain sanchez on 11/11/14.
+//  Copyright (c) 2014 alain sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestSubidaGit : NSObject
+
+@end
---
+++ Git test/TestSubidaGit.m
@@ -0,0 +1,13 @@
+//
+//  TestSubidaGit.m
+//  Git test
+//
+//  Created by alain sanchez on 11/11/14.
+//  Copyright (c) 2014 alain sanchez. All rights reserved.
+//
+
+#import "TestSubidaGit.h"
+
+@implementation TestSubidaGit
+
+@end
